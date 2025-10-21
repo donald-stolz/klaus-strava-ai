ba03e2031cc09c76f2a85745c13d53123f95dc33
Don Stolz <donaldtstolz@gmail.com>
2025-10-20 19:17:52 -0500

    task: Add awaits and logging

--- api/main.py
+++ api/main.py
@@ -11,56 +11,29 @@ strava_client = StravaAPIClient(settings)
 gemini_client = GeminiAPIClient(settings)
 
 @app.post("/strava/webhook")
-def webhook(request: StravaWebhookEvent):
+async def webhook(request: StravaWebhookEvent):
     aspect_type = request.aspect_type
     object_type = request.object_type
+    print(f"Aspect type: {aspect_type}, Object type: {object_type}")
     if aspect_type != "create" or object_type != "activity":
         return
     
     activity_id = request.object_id
-    activity = strava_client.get_activity(activity_id)
-    # TODO: Handle the activity
+    print(f"Activity ID: {activity_id}")
+    activity = await strava_client.get_activity(activity_id)
+    print(f"Activity: {activity}")
+
     if activity.distance < 1000:
-        strava_client.hide_activity(activity_id)
+        await strava_client.hide_activity(activity_id)
+        print(f"Activity hidden")
         return {"message": "Activity hidden"}
 
-    post = gemini_client.generate_post(activity)
-    strava_client.update_activity(activity_id, post)
+    post = await gemini_client.generate_post(activity)
+    await strava_client.update_activity(activity_id, post)	
+    print(f"Post generated")
     return {"message": "Post generated"}
 
 @app.get("/strava/webhook")
 def webhook_get(request: Request):
     challenge = request.query_params["hub.challenge"]
     return {"hub.challenge": challenge}
-
-# Testing endpoints
-# @app.get("/")
-# def read_root():
-#     return {"message": "Hello World"}
-
-# @app.get("/strava/athlete")
-# def get_athlete():
-#     return strava_client.get_athlete()
-
-# @app.get("/strava/activities")
-# def get_activities():
-#     return strava_client.get_activities()
-
-# @app.get("/strava/activity/{activity_id}")
-# def get_activity(activity_id: str):
-#     return strava_client.get_activity(activity_id)
-
-# @app.put("/strava/activity/{activity_id}")
-# def update_activity(activity_id: str, activity: StravaUpdatableActivity):
-#     return strava_client.update_activity(activity_id, activity)
-
-# @app.put("/strava/activity/{activity_id}/hide")
-# def hide_activity(activity_id: str):
-#     return strava_client.hide_activity(activity_id)
-
-# @app.put("/gemini/post/{activity_id}")
-# def generate_post(activity_id: str):
-#     activity = strava_client.get_activity(activity_id)
-#     post = gemini_client.generate_post(activity)
-#     strava_client.update_activity(activity_id, post)
-#     return post
